f8525b2437319cd09000ff75e3faf5591e7c24d9
Ilya Kazlou <ilya02k@gmail.com>
2024-05-07 19:05:24 +0300

    swift 8

---
+++ Swift 8/Swift 8/Swift_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_8App.swift
+//  Swift 8
+//
+//  Created by Ann Dosova on 7.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift 8/Swift 8UITests/Swift_8UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_8UITestsLaunchTests.swift
+//  Swift 8UITests
+//
+//  Created by Ann Dosova on 7.05.24.
+//
+
+import XCTest
+
+final class Swift_8UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
